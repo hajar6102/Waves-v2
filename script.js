26a4b023803422efa93be8e74d9019fad6329e08
unknown <hajarkhalid2002@gmail.com>
2025-03-13 00:31:00 +0300

    wavse-v2

--- script.js
+++ script.js
@@ -19,3 +19,25 @@ function toggleContent() {
         visionContent.classList.add('active');
     }
 }
+//blog script
+var TrandingSlider = new Swiper('.blog-slider', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    loop: false,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+      rotate: 0,
+      stretch: 0,
+      depth: 100,
+      modifier: 2.5,
+    },
+    // pagination: {
+    //   el: '.swiper-pagination',
+    //   clickable: true,
+    // },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }
+  });
